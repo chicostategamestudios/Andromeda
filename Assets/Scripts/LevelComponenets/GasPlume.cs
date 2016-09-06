29dd7dfd1fd43cbe4255fdc22e749d50ceda0006
TJScorpion <tjscorpion93@gmail.com>
2016-09-06 16:50:52 -0700

    Working on gas plume

---
+++ Assets/Scripts/LevelComponenets/GasPlume.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GasPlume : MonoBehaviour {
+
+	ParticleSystem mySystem;
+	//add variable for easy adjusting
+
+
+	void GasBurst () {
+		mySystem = GetComponentInChildren<ParticleSystem> ();
+		mySystem.Play ();
+	}
+
+	void Awake () {
+		InvokeRepeating ("GasBurst", 1, 5);
+
+	}
+}
