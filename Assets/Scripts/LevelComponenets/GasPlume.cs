b37d55ebb4a850f51f097bf5b210705e751a0251
unknown <tjscorpion93@gmail.com>
2016-09-08 15:48:52 -0700

    TreeRoots complete
    Working on GasPlume

--- Assets/Scripts/LevelComponenets/GasPlume.cs
+++ Assets/Scripts/LevelComponenets/GasPlume.cs
@@ -5,15 +5,27 @@ public class GasPlume : MonoBehaviour {
 
 	ParticleSystem mySystem;
 	//add variable for easy adjusting
+	public float burstDelay;
+	public float plumeHeight;//particle lifetime
 
 
 	void GasBurst () {
 		mySystem = GetComponentInChildren<ParticleSystem> ();
+		mySystem.startLifetime = plumeHeight;
 		mySystem.Play ();
 	}
 
 	void Awake () {
-		InvokeRepeating ("GasBurst", 1, 5);
+		InvokeRepeating ("GasBurst", 1, burstDelay);
 
+	}
+
+	void OnTriggerStay(Collider other){
+		//check if GasBurst == true
+		//and if "fire" object is near
+
+		if (other.gameObject.tag == "fire" && mySystem.isPlaying) {
+			//ignite
+		}
 	}
 }
