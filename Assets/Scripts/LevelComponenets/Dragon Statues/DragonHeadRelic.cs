fed93cb16373879c33c54300714fad4f05674886
zacharycoon <zachary.coon2@gmail.com>
2016-09-13 16:49:44 -0700

    Bridge is completed. Relic statue is completed. All other statues are started and waiting on some assests to test.

--- Assets/Scripts/LevelComponenets/Dragon Statues/DragonHeadRelic.cs
+++ Assets/Scripts/LevelComponenets/Dragon Statues/DragonHeadRelic.cs
@@ -5,7 +5,7 @@ public class DragonHeadRelic : MonoBehaviour {
     
     public static bool isTempleActive; //Global Varible to tell the rest of the game that the temple is active. If you need to call it in anouther script, use 'DragonHeadRelic.isTempleActive
     [Tooltip("Use this varible to make the temple 'Activate' the rest of the level. If you need to call it in anouther script, use 'DragonHeadRelic.isTempleActive")]
-    public bool isTempleActive_; // used to be able to manipulate the global varible while playtesting game
+    public bool isTempleActive_; // used to be able to manipulate the global varible while playtesting game (local varible)
     SphereCollider myTrigger;
     public GameObject relic;
 
@@ -17,13 +17,18 @@ public class DragonHeadRelic : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
+    // makes the global variable true if the local varible is set as true and vice versa
 	void Update () {
         isTempleActive = isTempleActive_;
 	}
 
-    void OnTriggerEnter() {
-        isTempleActive_ = true;
-        Destroy(relic);
-        Destroy(myTrigger);
+    // if the player collides with the relic, then the temple activates
+    // also destroys the relic and the trigger
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == ("Player")) {
+            isTempleActive_ = true;
+            Destroy(relic);
+            Destroy(myTrigger);
+        }
     }
 }
---
+++ Assets/Scripts/LevelComponenets/Dragon Statues/FireballDragonStatue.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireballDragonStatue : MonoBehaviour {
+    [Tooltip("Override for playtesting (or other reasons). Make this true if you want to activate it before the relic is grabbed")]
+    public bool isActive;
+    [Tooltip("number of seconds in between fireballs")]
+    public float waitTime;
+    [Tooltip("intial delay of activation after artifact has been collected (in seconds)")]
+    public float initialDelay;
+
+    bool activated = false;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((isActive || DragonHeadRelic.isTempleActive) && !activated)
+        {
+            activated = true;
+            InvokeRepeating("SpawnFireball", initialDelay, waitTime);
+        }
+    }
+
+    void SpawnFireball()
+    {
+
+
+    }
+}
