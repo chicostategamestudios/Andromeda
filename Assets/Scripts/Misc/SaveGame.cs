a27c5900483819ef2143c2252840a9aefedae937
vaccaris <vaccari.steven@gmail.com>
2016-09-13 16:45:16 -0700

    update to save multiple game saves, example with loading files, udpate Health to have a public death function that just calls death on the Death script

--- Assets/Scripts/Misc/SaveGame.cs
+++ Assets/Scripts/Misc/SaveGame.cs
@@ -30,20 +30,28 @@ public enum LevelGrade{
 	S
 }
 
-
+public enum MyLoadedGame
+{
+	GameOne,
+	GameTwo,
+	GameThree
+}
 
 public class SaveGame : MonoBehaviour { //This class will save the game.
-   string saveDataFile = "/SaveData.dat";
-   public GameStats loadStats;
-   public static SaveGame GameSaver;
+	string saveDataFile = "/SaveData";
+	string saveDataFileEnding = ".dat";
+	private static MyGames _myGames;
+	private static GameStats myGame;
+	private static MyLoadedGame curGame;
+	private static SaveGame GameSaver;
 
     void Awake()
-    {
+	{ //this setup is to ensure a singleton of the object. We only need one gamesaver
    
         if(GameSaver == null)
         {
             GameSaver = this.gameObject.GetComponent<SaveGame>();
-           ;
+           
             
         }
         else
@@ -55,76 +63,104 @@ public class SaveGame : MonoBehaviour { //This class will save the game.
 
     }
 
+	public static SaveGame GetGameSaver{ //this is my getter to get this object
+		get {return GameSaver;}
+	}
 
-    public void UpdateSave(LevelToUnlock level, LockMode lockType) //this function will be called to update the save file. Just pass in what level you want to update
+	public MyGames GetMyGames(){ //this will return my games, we SHOULD be loading games before we return myGames....
+		if (_myGames != null) {
+			return _myGames;
+		} else {
+			LoadGames (); //....but just in case we forgot to load games first, load games and then return them to the player 
+			return _myGames;
+		}
+
+	}
+
+	public GameStats GetGameStats{ //this contains the value of our currently selected game. 
+		get {
+			if (myGame == null) {
+				Debug.LogError ("Trying to GetGameStats without setting a game first. make sure you load in and set a game before you run GetGameStats");
+			}
+
+			return myGame; }
+
+	}
+
+
+	public void LoadGames() //this should be ran in the beginning of the game
+	{
+
+		MyGames curGames = new MyGames(); //creates a new MyGames object which stores all of the data for each Game
+
+		if (File.Exists(Application.persistentDataPath + saveDataFile + saveDataFileEnding)) //check if we have a file to open (the game has been played before)
+		{
+			BinaryFormatter loadBf = new BinaryFormatter(); //open a BinaryFormatter
+			FileStream loadFS = File.Open(Application.persistentDataPath + saveDataFile + saveDataFileEnding, FileMode.Open); //Open a file in our persistent Data Path location
+			curGames = (MyGames)loadBf.Deserialize(loadFS); //Deserialize our saved game dat file and store it into curStats
+		}
+		else //if we don't have a game save, create a new one
+		{
+			
+			curGames.GameOne = CreateNewSave ();
+			curGames.GameTwo = CreateNewSave ();
+			curGames.GameThree = CreateNewSave ();
+
+			_myGames = curGames;
+
+			Save(); //save the new game as a new file
+
+		}
+
+		_myGames = curGames; 
+	}
+
+	public void SetGame(MyLoadedGame myNewGame){ //we call this function through the UI to set the game that we are currently playing
+		GameStats getGame = new GameStats();
+
+		if (myNewGame == MyLoadedGame.GameOne) {
+			myGame = _myGames.GameOne;
+			curGame = MyLoadedGame.GameOne;
+		} else if (myNewGame == MyLoadedGame.GameTwo) {
+			myGame = _myGames.GameTwo;
+			curGame = MyLoadedGame.GameTwo;
+		} else if (myNewGame == MyLoadedGame.GameThree) {
+			myGame = _myGames.GameThree;
+			curGame = MyLoadedGame.GameThree;
+		}
+
+		//Debug.Log (curGame);
+
+	}
+
+	public void UpdateSave(GameStats UpdateGame) //this function will be called to update the save file. Just pass in what level you want to update
         //and how you want to update it. This will need to be expanded later on (and potentially overloaded) when we start recording more stats
     {
-        if (GameSaver == null)
-        {
-            this.gameObject.GetComponent<SaveGame>();
-
-        }
-
-
-       // if (loadStats == null) //make sure we load the game at least once before saving, so we're not setting null values
-       // {
-       //     LoadGame(); //LoadGame should be ran during the opening of the game, this is just a failsafe
-       // }
-
-        LevelStats newStats = new LevelStats(); //we create a new stats object.
-        newStats.thisLevel = level; //give the new object the level that we passed in
-        newStats.locked = lockType; //as well as how we want this level to be locked
+      
+       // LevelStats newStats = new LevelStats(); //we create a new stats object.
+       // newStats.thisLevel = level; //give the new object the level that we passed in
+       // newStats.locked = lockType; //as well as how we want this level to be locked
 
         //later we will set more stats here
 
-        switch (level) //this switch is a little dirty, but not too bad. It could be switched later with a more dynamic system but given the current scope of the game this works fine
-        {
-            case LevelToUnlock.tutorial:
-                loadStats.TutorialLevelStats = newStats; //set the stats of whichever level we're working on
-                break;
-            case LevelToUnlock.levelOne:
-                loadStats.LevelOneStats = newStats;
-                break;
-            case LevelToUnlock.levelTwo:
-                loadStats.LevelTwoStats = newStats;
-                break;
-            case LevelToUnlock.levelThree:
-                loadStats.LevelThreeStats = newStats;
-                break;
-            case LevelToUnlock.levelFour:
-                loadStats.LevelFourStats = newStats;
-                break;
+		myGame = UpdateGame;
 
-            default:
-                Debug.LogError("Trying to save an invalid value");
-                break;
+		if (curGame == MyLoadedGame.GameOne) {
+			_myGames.GameOne = UpdateGame;
+		} else if (curGame == MyLoadedGame.GameTwo) {
+			_myGames.GameTwo = UpdateGame;
+		} else if (curGame == MyLoadedGame.GameThree) {
+			_myGames.GameThree = UpdateGame;
+		}
 
-        }
+		Debug.Log (curGame);
 
         Save(); //update the save file after we have update the gamestats
 
     }
 
-    public void LoadGame() //this should be ran in the beginning of the game
-    {
+	
 
-        GameStats curStats = new GameStats(); //create a new stats object, which contains all of the stats of our game
-        if (File.Exists(Application.persistentDataPath + saveDataFile)) //check if we have a file to open
-        {
-            BinaryFormatter loadBf = new BinaryFormatter();
-            FileStream loadFS = File.Open(Application.persistentDataPath + saveDataFile, FileMode.Open);
-            curStats = (GameStats)loadBf.Deserialize(loadFS);
-        }
-        else
-        {
-            curStats = CreateNewSave();
-            loadStats = curStats;
-            Save();
-
-        }
-       
-        loadStats = curStats; 
-    }
 
    
 
@@ -132,17 +168,24 @@ public class SaveGame : MonoBehaviour { //This class will save the game.
     public void Save() //this should be ran by the UpdateSave function. After we have update some data we will write it to our save file
     {
        
-
         BinaryFormatter saveBf = new BinaryFormatter(); //open a formatter
-        FileStream saveFile = File.Create(Application.persistentDataPath + saveDataFile); //open the file      
-        saveBf.Serialize(saveFile, loadStats); //save the object that we loaded previously. This should have stats updated before we run save saves.
+		FileStream saveFile = File.Create(Application.persistentDataPath + saveDataFile + saveDataFileEnding); //open the file      
+		//Debug.Log(Application.persistentDataPath + saveDataFile + saveDataFileEnding);
+		saveBf.Serialize(saveFile, _myGames); //save the object that we loaded previously. This should have stats updated before we run save saves.
         saveFile.Close();                                   
     }
 
-    public GameStats CreateNewSave()
-    {
+
+
+
+
+    public GameStats CreateNewSave() //this will create a new save. This creates a new game save object with 
+    {								//all values set to default values. There is probably a better way to achieve this
         GameStats newGame = new GameStats();
         LevelStats newGameStats = new LevelStats();
+
+		newGame.existingFile = false;
+		newGame.PlayTime = 0f;
 
         newGameStats.thisLevel = LevelToUnlock.tutorial;
         newGameStats.locked = LockMode.locked;
@@ -164,15 +207,28 @@ public class SaveGame : MonoBehaviour { //This class will save the game.
         newGameStats.locked = LockMode.locked;
         newGame.LevelFourStats = newGameStats;
 
-        return newGame;
+        return newGame; //once our new game is created, return it
     }
 
 
 }
 
 [Serializable]
+public class MyGames //My Games is the parent object that is being saved, it contains all of the the players current games. Create and 
+					//save an empty one of these to write over the game
+{
+	public GameStats GameOne;
+	public GameStats GameTwo;
+	public GameStats GameThree;
+}
+	
+[Serializable]
 public class GameStats //this object holds all of our game stats. we can store things like level stats or other misc achievements here
 {
+	public bool existingFile;
+
+	public float PlayTime;
+
     public LevelStats TutorialLevelStats;
     public LevelStats LevelOneStats;
     public LevelStats LevelTwoStats;
