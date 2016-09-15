aeed5376fdf97377663b1eacae90517cb25f8140
unknown <tjscorpion93@gmail.com>
2016-09-15 15:50:27 -0700

    Finishing firebeam

---
+++ Assets/Scripts/LevelComponenets/FlamePillar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlamePillar : MonoBehaviour {
+
+	public bool isActive;
+	Renderer beamRenderer;
+
+	// Use this for initialization
+	void Start () {
+		isActive = false;
+		beamRenderer = GetComponentInChildren<MeshRenderer> ();
+		beamRenderer.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isActive) {
+			FireBeam ();
+		}
+	}
+
+	void FireBeam(){
+
+	}
+}
