091313d78760a87cc6c6ebf4a2d4e195a4986ce8
unknown <tim_chen706@yahoo.com>
2016-09-20 10:06:53 -0700

    library cleared
    zachary's assets added in

---
+++ Assets/Scripts/LevelComponenets/BurningBridge.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BurningBridge : MonoBehaviour {
+    public Transform bridge;
+    BoxCollider myTrigger;
+
+    // Use this for initialization
+    void Start () {
+        myTrigger = this.gameObject.GetComponent<BoxCollider>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)  {
+
+
+        if (col.gameObject.tag == ("Player"))
+        {
+            InvokeRepeating("Fall", 0.01f, 0.01f);
+            Destroy(myTrigger);
+
+        }
+    }
+
+    void Fall() {
+        bridge.Rotate(Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/LevelComponenets/Dragon Statues/DragonHeadRelic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonHeadRelic : MonoBehaviour {
+     
+    public static bool isTempleActive; //Global Varible to tell the rest of the game that the temple is active. If you need to call it in anouther script, use 'DragonHeadRelic.isTempleActive
+    [Tooltip("Use this varible to make the temple 'Activate' the rest of the level. If you need to call it in anouther script, use 'DragonHeadRelic.isTempleActive")]
+    public bool isTempleActive_; // used to be able to manipulate the global varible while playtesting game (local varible)
+    SphereCollider myTrigger;
+    public GameObject relic;
+
+	// Use this for initialization
+	void Start () {
+        isTempleActive = false;
+        isTempleActive_ = false;
+        myTrigger = this.gameObject.GetComponent<SphereCollider>();
+	}
+	
+	// Update is called once per frame
+    // makes the global variable true if the local varible is set as true and vice versa
+	void Update () {
+        isTempleActive = isTempleActive_;
+	}
+
+    // if the player collides with the relic, then the temple activates
+    // also destroys the relic and the trigger
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == ("Player")) {
+            isTempleActive_ = true;
+            Destroy(relic);
+            Destroy(myTrigger);
+        }
+    }
+}
