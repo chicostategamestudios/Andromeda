67fb741e3e3e43345806563e15f63435aff11d65
xela10104 <aglaeser@mail.csuchico.edu>
2016-09-22 15:51:04 -0700

    Commenting out code and small touch ups to code

--- Assets/Scripts/LevelComponenets/Lavguanas.cs
+++ Assets/Scripts/LevelComponenets/Lavguanas.cs
@@ -4,48 +4,47 @@ using System;
 
 public class Lavguanas : MonoBehaviour
 {
-
-    bool grounded;
-    public float speed = 3;
-    public float randomTimeCheck;
-    public float changeDirChance;
-    public float damage;
-    float forward = 1;
-    public float wallDist = 0.6f;
-    float verticleSpeed;
-    float grav = 10;
-    float distToGround;
-    GameObject Player;
-    Renderer rend;
-    private Color lerpedColor = Color.white;
-    float fireRate = 5;
-    float lerpTime = 0;
-    public GameObject pellet;
-    private GameObject clone;
-    int playerDirection;
-    public int fireDirection;
+    float speed = 3; //speed at which the AI moves
+    float forward = 1; //determines which way is forward. 1 is right, -1 is left
+    public float wallDist = 0.6f; //distance to the wall
+    float verticleSpeed; //speed at which the AI falls
+    float grav = 10; //gravity intensity
+    float distToGround; //distance to the ground from the cetner of the AI
+    GameObject Player; // referance to the player
+    Renderer rend; //referance to the renderer
+    private Color lerpedColor = Color.white; //the color that the AI lerps between
+    float fireRate = 5; //rate at which the AI fires at
+    float lerpTime = 0; //time it takes to lerp
+    public GameObject pellet; // ref to the pellet Prefab
+    private GameObject clone; //ref to spawned pellet
+    int playerDirection; //variable to hold the direction of the player
+    public int fireDirection; //variable to check the direction the Ai fires in
 
     // Use this for initialization
     void Start()
     {
+        //sets the renderer, player, and distToGround components
         rend = GetComponent<Renderer>();
         Player = GameObject.FindGameObjectWithTag("Player");
         distToGround = GetComponent<Collider>().bounds.extents.y;
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        //if there is no ref to player, add ref to player
         if (Player == null)
         {
             Player = GameObject.FindGameObjectWithTag("Player");
         }
         Assets.Scripts.Character.CharController charController = Player.GetComponent<Assets.Scripts.Character.CharController>();
+        //checks where the player is in relation to itself. This checks if the player is to the left of itself
         if (Player.transform.position.x < transform.position.x)
         {
+            //checks if the players facing right. The previous if statement and this check if the player is looking at the AI from the left
             if (charController.lastDir > 0)
             {
+                //turns off the AI's renderer making it invisible, slows it down, and reduces the timer on its fire timer
                 rend.enabled = false;
                 speed = 1;
                 if (lerpTime > 0)
@@ -54,8 +53,10 @@ public class Lavguanas : MonoBehaviour
                 }
 
             }
+            //if the player is facing away from AI while to the left of the AI
             else
             {
+                //turns on the renderer, speed it up, lerps the color of the AI from white to red, sets the direction to fire in, starts timer to fire
                 rend.enabled = true;
                 speed = 3;
                 lerpedColor = Color.Lerp(Color.white, Color.red, lerpTime);
@@ -71,10 +72,13 @@ public class Lavguanas : MonoBehaviour
                 rend.material.color = lerpedColor;
             }
         }
+        //if the player is to the right of the AI
         else
         {
+            //if the player is facing away while to the right of the AI
             if (charController.lastDir > 0)
             {
+                //turns on the renderer, speed it up, lerps the color of the AI from white to red, sets the direction to fire in, starts timer to fire
                 rend.enabled = true;
                 speed = 3;
                 lerpedColor = Color.Lerp(Color.white, Color.red, lerpTime);
@@ -90,9 +94,11 @@ public class Lavguanas : MonoBehaviour
                 rend.material.color = lerpedColor;
 
             }
+            //if the player is facing the AI while to the right of the AI
             else
             {
                 {
+                    //turns off the AI's renderer making it invisible, slows it down, and reduces the timer on its fire timer
                     rend.enabled = false;
                     speed = 1;
                     if (lerpTime > 0)
@@ -104,7 +110,7 @@ public class Lavguanas : MonoBehaviour
             }
 
         }
-        /*
+        //raycast from the AI to directly underneath it to check if there is ground, if so verticalSpeed is 0 otherwise add gravity to vertical speed
         if (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f))
         {
             verticleSpeed = 0;
@@ -113,18 +119,18 @@ public class Lavguanas : MonoBehaviour
         {
             verticleSpeed -= grav * Time.deltaTime;
         }
-
+        //move forward over time
         Vector3 moveVector = new Vector3(speed * forward, verticleSpeed, 0f);
         transform.Translate(moveVector * Time.deltaTime);
         RaycastHit hit;
+        //raycast forward to detect a wall while ignoring the player, if true turn around
         if (Physics.Raycast(transform.position, Vector3.right * forward, out hit, wallDist) && hit.collider.gameObject.tag != "Player")
         {
             forward *= -1f;
         }
-    */
     }
 
-
+    //fire a small pellet forward. Restarts the time to fire.
     void fire()
     {
         if (fireDirection > 0)
--- Assets/Scripts/LevelComponenets/MoltenCrab.cs
+++ Assets/Scripts/LevelComponenets/MoltenCrab.cs
@@ -14,11 +14,12 @@ public class MoltenCrab : MonoBehaviour {
 	CharacterController mycont;
 	float grav = 10;
     float distToGround;
+    public float ledgeDrop = 5f;
 
 	// Use this for initialization
 	void Start () {
-        //mycont = transform.gameObject.AddComponent<CharacterController> ();
-        //mycont.height = 1f;
+        //sets distToGround to the distance from the center of the AI to its bottom
+        //starts the function to randomly have the AI change directions. 
         distToGround = GetComponent<Collider>().bounds.extents.y;
 		if (randomTimeCheck > 0) {
 			InvokeRepeating("ChangeDir", randomTimeCheck, randomTimeCheck);
@@ -28,27 +29,28 @@ public class MoltenCrab : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        //raycast distance distToGround plus .1 to check if there is floor. If true, there is floor and verticalSpeed is 0
         if (Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f))
         {
             verticleSpeed = 0;
+            //raycast downward from a short distance in front of the AI, goes a distance specified by distToGround + ledgeDrop. If this returns true there is a ledge and the AI can drop down, if not there is no ledge and the AI turns around. ledgeDrop can be changed to allow the AI to drop down greater distances.
+            if (Physics.Raycast(new Vector3(transform.position.x + forward, transform.position.y - .25f, transform.position.z), -Vector3.up, distToGround + ledgeDrop))
+            {
+                Debug.Log("ledge");
+            }
+            else
+            {
+                Debug.Log("no ledge");
+                forward *= -1f;
+            }
         }
         else
         {
+            //if there is no floor under the AI add gravity to it.
             verticleSpeed -= grav * Time.deltaTime;
         }
 
-
-
-        /*grounded = mycont.isGrounded;
-
-		if (grounded) {
-			verticleSpeed = 0;
-		} else {
-			verticleSpeed -= grav * Time.deltaTime;
-		}
-        */
-
-
+        //move forward and raycast forward to check for walls while ignoring the player. if raycast hits, turn the ai around
         Vector3 moveVector = new Vector3 (speed * forward, verticleSpeed, 0f);
 		transform.Translate (moveVector * Time.deltaTime);
         RaycastHit hit;
@@ -57,8 +59,9 @@ public class MoltenCrab : MonoBehaviour {
 		}
 
 	}
-	void ChangeDir(){
-		float chance = Random.Range (0, 100);
+    //Set the value "changeDirChance" in inspector from 0 - 100 to give determine the chance of it changing direction when this is called.
+    void ChangeDir(){
+        float chance = Random.Range (0, 100);
 		if (chance < changeDirChance) {
 			forward *= -1f;
 		}
