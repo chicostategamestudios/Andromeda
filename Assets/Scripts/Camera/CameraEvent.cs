64da3483821a527d218e48418459f46cb5b9e732
jdeal2 <jdeal2@mail.csuchico.edu>
2016-09-27 16:21:43 -0700

    Updated Camera 9/27

--- Assets/Scripts/Camera/CameraEvent.cs
+++ Assets/Scripts/Camera/CameraEvent.cs
@@ -2,30 +2,33 @@
 using System.Collections;
 
 namespace Assets.Scripts.Components{
-public class CameraEvent : MonoBehaviour {
+	public class CameraEvent : MonoBehaviour {
 
-	public Transform focusLock;
-	public int cameraEvent;
+		public Transform focusLock;
+		public int cameraEvent;
+		public float eventSmooth;
 
-	//private GameObject cameraMain;
+		//private GameObject cameraMain;
 
-	void Awake () {
-		//cameraMain = GameObject.FindWithTag ("MainCamera");
-	}
+		void Awake () {
+			//cameraMain = GameObject.FindWithTag ("MainCamera");
+		}
 
-	void OnTriggerEnter (Collider col) {
-		if (col.tag == "Player") {
-			if (cameraEvent == 0) {
-				_Camera.cameraEvent = cameraEvent;
-			}
-			if (cameraEvent > 0 && cameraEvent < 4) {
-				_Camera.cameraEvent = cameraEvent;
-				_Camera.focusLock = focusLock;
-			}
-			if (cameraEvent > 3) {
-				Debug.LogError ("Unknown camera event #. Please input a number between 0 and 3");
+		void OnTriggerEnter (Collider col) {
+			if (col.tag == "Player") {
+				if (cameraEvent == 0) {
+					_Camera.cameraEvent = cameraEvent;
+					_Camera.eventSmooth = eventSmooth;
+				}
+				if (cameraEvent > 0 && cameraEvent < 4) {
+					_Camera.cameraEvent = cameraEvent;
+					_Camera.focusLock = focusLock;
+					_Camera.eventSmooth = eventSmooth;
+				}
+				if (cameraEvent > 3 || cameraEvent < 0) {
+					Debug.LogWarning ("Unknown camera event #. Please input a number between 0 and 3");
+				}
 			}
 		}
 	}
-}
 }
