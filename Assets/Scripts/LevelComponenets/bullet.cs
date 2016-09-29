63bf92edf91af6ee251e0b9a50d4436a7560e74e
jackojackofever <jrxwhite@gmail.com>
2016-09-29 15:58:19 -0700

    Added in turret and platforms

---
+++ Assets/Scripts/LevelComponenets/bullet.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+
+    public float bulletSpd = 1f;
+    public float lifetime = 2f;
+
+    void Awake()
+    {
+        Destroy(gameObject, lifetime);
+    }
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(bulletSpd, 0, 0);
+	}
+}
